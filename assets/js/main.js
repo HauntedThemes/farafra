502f50845268084c6785527fc4924f20967e7203
Cristian Nistor <Cristian.Nistor@everymatrix.com>
2018-01-23 17:46:28 +0200

    Second commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,9 +7,42 @@ jQuery(document).ready(function($) {
     var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0),
         h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
 
+    var config = {
+        'share-selected-text': true,
+        'load-more': false,
+        'infinite-scroll': false,
+        'infinite-scroll-step': 3,
+        'disqus-shortname': 'hauntedthemes-demo'
+    };
+
 	var swiper = new Swiper('.swiper-container', {
 		slidesPerView: 1,
 		spaceBetween: 30,
     });
 
+    // Initialize Disqus comments
+    if ($('#content').attr('data-id') && config['disqus-shortname'] != '') {
+
+        $('.comments-trigger').on('click', function(event) {
+            event.preventDefault();
+
+            if (!$('#disqus_thread').length) {
+	            $('.comments').append('<div id="disqus_thread"></div>').addClass('active');
+
+	            var url = [location.protocol, '//', location.host, location.pathname].join('');
+	            var disqus_config = function () {
+	                this.page.url = url;
+	                this.page.identifier = $('#content').attr('data-id');
+	            };
+
+	            (function() {
+	            var d = document, s = d.createElement('script');
+	            s.src = '//'+ config['disqus-shortname'] +'.disqus.com/embed.js';
+	            s.setAttribute('data-timestamp', +new Date());
+	            (d.head || d.body).appendChild(s);
+	            })();
+            };
+        });
+
+    };
 });
