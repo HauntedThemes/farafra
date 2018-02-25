39bfc75a355c703b2e1c7f0fc12dcfe53dee768e
Cristian Nistor <contact@nistorcristian.com>
2018-02-26 01:47:03 +0200

    Prepare for release 3

--- farafra/assets/js/libraries.js
+++ farafra/assets/js/libraries.js
@@ -1,3 +1,25 @@
+if (typeof Object.assign != 'function') {
+  Object.assign = function(target) {
+    'use strict';
+    if (target == null) {
+      throw new TypeError('Cannot convert undefined or null to object');
+    }
+
+    target = Object(target);
+    for (var index = 1; index < arguments.length; index++) {
+      var source = arguments[index];
+      if (source != null) {
+        for (var key in source) {
+          if (Object.prototype.hasOwnProperty.call(source, key)) {
+            target[key] = source[key];
+          }
+        }
+      }
+    }
+    return target;
+  };
+}
+
 /*
  Copyright (C) Federico Zivolo 2017
  Distributed under the MIT License (license terms are at http://opensource.org/licenses/MIT).
